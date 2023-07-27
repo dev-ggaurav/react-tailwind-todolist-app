6980477a2d465091ccbb93795b70e32ddfe793ba
dev-ggaurav <devggaurav@gmail.com>
2023-07-27 08:31:55 +0530

    style change

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -8,7 +8,7 @@ function InputForm(props) {
         onChange={props.handleChange}
         value={props.newTask}
       />
-      <button className="w-full py-3 mt-4 font-medium text-white bg-indigo-600 hover:bg-indigo-500 rounded-md hover:shadow">
+      <button className="w-full py-3 mt-4 font-medium text-white bg-slate-900 hover:bg-slate-600 rounded-md hover:shadow">
         Add New Task
       </button>
     </form>
